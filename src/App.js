f721632794b3a6fc50ed3b26c325f2ce5171ac4c
bLitz <ihassansajjad@gmail.com>
2024-10-31 17:09:08 +0500

    changed 128kmac location

--- src/App.js
+++ src/App.js
@@ -174,7 +174,7 @@ function App() {
           </p>
         </div>
         <div className="video-container">
-          <video src={mac_vid} autoPlay loop muted playsInline type="video/mp4" className="app-video"></video>
+          <video src="/videos/128kmac.mp4" autoPlay loop muted playsInline type="video/mp4" className="app-video"></video>
         </div>
         <div className="dock">
           <div className="dock-item">
